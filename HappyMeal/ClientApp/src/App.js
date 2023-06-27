bee9074c5aa69313edd177504982075c10b54335
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-06-27 11:31:40 +0300

    Created Header component with css styling

--- HappyMeal/ClientApp/src/App.js
+++ HappyMeal/ClientApp/src/App.js
@@ -1,9 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home.js'
+import { Header } from './components/Header/Header.js';
 
 function App() {
   return (
     <div id='box'>
+      <Header />
+
       <main id='main-content'>
         <Routes>
           <Route path='/' element={<Home />}></Route>
--- HappyMeal/clientapp/src/components/Header/Header.js
+++ HappyMeal/clientapp/src/components/Header/Header.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
+import { faBurger } from '@fortawesome/free-solid-svg-icons';
 
+import '../reset.css';
 import './Header.css';
 
 export const Header = () => {
     return(
         <header>
-        <h1><Link className='home' to='/'>HappyMeal</Link></h1>
+        <h1><Link className='home' to='/'><FontAwesomeIcon icon={faBurger} />HappyMeal</Link></h1>
         <nav>
-            <Link to='/bacomeRestaurateur'>Become Restaurateur</Link>
+            <Link to='/becomeRestaurateur'><FontAwesomeIcon icon={faMotorcycle} />Become Restaurateur</Link>
+            <Link to='/home/barsMenu'><FontAwesomeIcon icon={faBars} /></Link>
         </nav>
         </header>
     )
